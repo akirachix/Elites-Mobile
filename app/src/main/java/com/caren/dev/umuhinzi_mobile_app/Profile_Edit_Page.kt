8c3566e07191343b16ae8f38c94220738768a50e
Caren Chepngetich <chepngetichkosgeicarenn@gmail.com>
2024-07-16 07:49:02 +0300

    Add changes

--- app/src/main/java/com/caren/dev/umuhinzi_mobile_app/Profile_Edit_Page.kt
+++ app/src/main/java/com/caren/dev/umuhinzi_mobile_app/Profile_Edit_Page.kt
@@ -1,9 +1,9 @@
 package com.caren.dev.umuhinzi_mobile_app
 
-import android.content.Intent
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.caren.dev.umuhinzi_mobile_app.databinding.ActivityMain8Binding
+//import android.content.Intent
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//import com.caren.dev.umuhinzi_mobile_app.databinding.ActivityMain8Binding
 
 //class Profile_Edit_Page : AppCompatActivity() {
 //    lateinit var binding: Profile_Edit_Page
