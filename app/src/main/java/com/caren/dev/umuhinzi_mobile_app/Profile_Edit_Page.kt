8c4b844810b552a390498a822f7180013014b779
Caren Chepngetich <chepngetichkosgeicarenn@gmail.com>
2024-07-16 07:48:04 +0300

    Add changes

--- app/src/main/java/com/caren/dev/umuhinzi_mobile_app/Profile_Edit_Page.kt
+++ app/src/main/java/com/caren/dev/umuhinzi_mobile_app/Profile_Edit_Page.kt
@@ -5,22 +5,22 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.caren.dev.umuhinzi_mobile_app.databinding.ActivityMain8Binding
 
-class Profile_Edit_Page : AppCompatActivity() {
-    lateinit var binding: ActivityMain8Binding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMain8Binding.inflate(layoutInflater)
-
-        setContentView(binding.root)
-
-        binding.btnEditDetails.setOnClickListener{
-            val intent = Intent(this, Profile_Page::class.java)
-            startActivity(intent)
-        binding.etSave.setOnClickListener{
-            val intent = Intent(this, Profile_Page::class.java)
-            startActivity(intent)
-        }
-        }
-
-    }
-}
+//class Profile_Edit_Page : AppCompatActivity() {
+//    lateinit var binding: Profile_Edit_Page
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding = Profile_Edit_Page.inflate(layoutInflater)
+//
+//        setContentView(binding.root)
+//
+//        binding.btnEditDetails.setOnClickListener{
+//            val intent = Intent(this, Profile_Page::class.java)
+//            startActivity(intent)
+//        binding.etSave.setOnClickListener{
+//            val intent = Intent(this, Profile_Page::class.java)
+//            startActivity(intent)
+//        }
+//        }
+//
+//    }
+//}
