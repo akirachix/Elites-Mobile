ba7ba84deb1b77474c5b00976e732c516fb1e41f
Linet Ndau <mkandoelinet@gmail.com>
2024-07-17 10:50:59 +0300

    added activities

---
+++ app/src/main/java/com/caren/dev/umuhinzi_mobile_app/continuation_of_sign_upActivity.kt
@@ -0,0 +1,15 @@
+package com.caren.dev.umuhinzi_mobile_app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class continuation_of_sign_upActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        }
+    }
